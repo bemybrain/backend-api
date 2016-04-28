b96d7da35ef68c5e0da8929b093ebd9bb41870d2
obetoferreira <obetoferreira@gmail.com>
2016-04-27 23:02:16 -0300

    Enabling cors

--- server.js
+++ server.js
@@ -16,6 +16,11 @@ var app = express()
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser())
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
 
 // Passport
 require('./passport')(passport)
